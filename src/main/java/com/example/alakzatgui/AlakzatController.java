f3ce3e5bb71df8fc45636d75cc7e07bf01831522
SagiBeno <benosagi321@gmail.com>
2025-09-17 13:36:13 +0200

    Show listview data

--- src/main/java/com/example/alakzatgui/AlakzatController.java
+++ src/main/java/com/example/alakzatgui/AlakzatController.java
@@ -1,8 +1,10 @@
 package com.example.alakzatgui;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
@@ -10,7 +12,6 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 
@@ -19,6 +20,12 @@ public class AlakzatController {
     @FXML public ListView<String> listview_Listview;
     @FXML public ImageView imageview_Alakzat;
     @FXML public Pane pane_Alakzat;
+    @FXML public RadioButton radioPiros;
+    @FXML public RadioButton radioZold;
+    @FXML public RadioButton radioKek;
+    @FXML public RadioButton radioNegyzet;
+    @FXML public RadioButton radioKor;
+    @FXML public RadioButton radioHaromszog;
 
     public void onPirosSelected(ActionEvent actionEvent) {
         // System.out.println("onPirosSelected actionEvent" + actionEvent);
@@ -44,5 +51,33 @@ public class AlakzatController {
 
     public void onHaromszogSelected(ActionEvent actionEvent) throws FileNotFoundException {
         imageview_Alakzat.setImage(new Image("file:icons/haromszog.png"));
+    }
+
+    public void onHozaadClick(ActionEvent actionEvent) {
+        ObservableList<String> listviewLines = listview_Listview.getItems();
+
+        String newLine = "";
+
+        if (radioPiros.isSelected()) {
+            newLine += "Piros, ";
+        }
+
+        if (radioZold.isSelected()) {
+            newLine += "Zöld, ";
+        }
+
+        if (radioKek.isSelected()) {
+            newLine += "Kek, ";
+        }
+
+        // TODO
+        if (radioKor.isSelected()) newLine += "Kör";
+        if (radioHaromszog.isSelected()) newLine += "Háromszög";
+        if (radioNegyzet.isSelected()) newLine += "Négyzet";
+
+        //listviewLines.add("Piros, Kör");
+        if (!newLine.isEmpty()) listviewLines.add(newLine);
+        listview_Listview.setItems(listviewLines);
+        listview_Listview.getSelectionModel().selectLast();
     }
 }
