94ad8b405fd6d50b477dbf2857d403c42614669f
SagiBeno <benosagi321@gmail.com>
2025-09-20 11:07:40 +0200

    Ikon módosítása

--- src/main/java/com/example/alakzatgui/AlakzatApplication.java
+++ src/main/java/com/example/alakzatgui/AlakzatApplication.java
@@ -3,6 +3,7 @@ package com.example.alakzatgui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class AlakzatApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(AlakzatApplication.class.getResource("alakzat-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 480);
         stage.setTitle("Alakzatok");
+        stage.getIcons().setAll(new Image(AlakzatApplication.class.getResourceAsStream("negyzet.png")));
         stage.setScene(scene);
         stage.show();
     }
